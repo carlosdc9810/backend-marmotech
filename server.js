657688580e1eacfc03efad77a7fb74c5c31ce036
Carlosdc <davidcarlos9810@gmail.com>
2026-04-14 03:37:03 -0400

    fix cors production

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ const axios = require("axios");
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "https://frontend-marmotech.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: false
+}));
+
 app.use(express.json());
 app.use(express.static("public"));
 
