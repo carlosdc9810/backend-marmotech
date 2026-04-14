b84e16bc0a9260179d63e5352733ee77fd2d5f11
Carlosdc <davidcarlos9810@gmail.com>
2026-04-14 00:10:11 -0400

    Configurando Brevo SMTP

--- server.js
+++ server.js
@@ -11,10 +11,9 @@ const nodemailer = require("nodemailer");
 
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
-    family: 4, //  fuerza IPv4
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
